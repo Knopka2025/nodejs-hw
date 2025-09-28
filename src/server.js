1668cc4ba994d19fd5693551079a1df21be18582
Vlada <geazint6@gmail.com>
2025-09-28 12:27:12 +0300

    after checking

--- src/server.js
+++ src/server.js
@@ -1,16 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import 'dotenv/config'; 
 import pino from 'pino-http';
-
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3030;
 
 app.use(cors());
 app.use(express.json());
-app.use(pino());
 
 
 app.use(
@@ -29,44 +26,41 @@ app.use(
 	}),
 );
 
-// Реалізований маршрут GET /notes
+//  GET /notes
 app.get('/notes', (req, res) => {
-	console.log(req.body);
+
 	res.status(200).json({ message: 'Retrieved all notes' });
 });
 
-// Реалізований маршрут GET /notes/:noteId
+// GET /notes/:noteId
 app.get('/notes/:noteId', (req, res) => {
 	const noteId = req.params.noteId;
 
 	res.status(200).json({
-		message: `Retrieved note with ID: ${noteId}`
+		message: `Retrieved note with ID: ${noteId}`,
 	});
 });
 
-//Реалізований маршрут GET /test-error
+// GET /test-error
 app.get('/test-error', (req, res) => {
 	throw new Error('Simulated server error');
 });
 
-// Middleware для обробки помилок 404
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Base route is here' });
-});
+// обробка помилки 404
 
 app.use((req, res) => {
 	res.status(404).json({ message: 'Route not found' });
 });
 
-// Middleware для обробки помилок 500
-app.use((err, req, res, next) => {	//console.error('Error:', err.message);
+// обробка помилокки 500
+app.use((err, req, res, next) => {
 	res.status(500).json({
-		message: 'Simulated server error',
 
+		message: err.message || 'Internal Server Error',
 	});
 });
 
-// Запуск сервера
+// старт сервера
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
