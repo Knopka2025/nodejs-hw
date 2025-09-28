95788044de8940ad1a15cc04be5352525b6441f8
Vlada <geazint6@gmail.com>
2025-09-28 12:43:31 +0300

    after checking

--- src/server.js
+++ src/server.js
@@ -1,66 +1,64 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config'; 
 import pino from 'pino-http';
+import 'dotenv/config';
 
 const app = express();
-const PORT = process.env.PORT || 3030;
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
+const PORT = process.env.PORT ?? 3030;
 
 app.use(
-	pino({
-		level: 'info',
-		transport: {
-			target: 'pino-pretty',
-			options: {
-				colorize: true,
-				translateTime: 'HH:MM:ss',
-				ignore: 'pid,hostname',
-				messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-				hideObject: true,
-			},
-		},
-	}),
+    pino({
+        level: 'info',
+        transport: {
+            target: 'pino-pretty',
+            options: {
+                colorize: true,
+                translateTime: 'HH:MM:ss',
+                ignore: 'pid,hostname',
+                messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+                hideObject: true,
+            },
+        },
+    }),
 );
 
-//  GET /notes
-app.get('/notes', (req, res) => {
+// ✅ GET /
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Base route is here' });
+});
 
-	res.status(200).json({ message: 'Retrieved all notes' });
+// GET /notes
+app.get('/notes', (req, res) => {
+    console.log(req.body);
+    res.status(200).json({ message: 'Retrieved all notes' });
 });
 
 // GET /notes/:noteId
 app.get('/notes/:noteId', (req, res) => {
-	const noteId = req.params.noteId;
-
-	res.status(200).json({
-		message: `Retrieved note with ID: ${noteId}`,
-	});
+    const noteId = req.params.noteId;
+    res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
 
 // GET /test-error
 app.get('/test-error', (req, res) => {
-	throw new Error('Simulated server error');
+    throw new Error('Simulated server error');
 });
 
-// обробка помилки 404
-
+// Middleware 404
 app.use((req, res) => {
-	res.status(404).json({ message: 'Route not found' });
+    res.status(404).json({ message: 'Route not found' });
 });
 
-// обробка помилокки 500
+// Middleware 500
 app.use((err, req, res, next) => {
-	res.status(500).json({
-
-		message: err.message || 'Internal Server Error',
-	});
+    res.status(500).json({ message: 'Simulated server error' });
 });
 
-// старт сервера
+// Start server
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
