7aa09c14ce91ed7ec493bce865511efc5091e62f
Vlada <geazint6@gmail.com>
2025-09-28 06:38:06 +0300

    server

--- src/server.js
+++ src/server.js
@@ -12,30 +12,61 @@ app.use(cors());
 app.use(express.json());
 app.use(pino());
 
-// Routes
+
+app.use(
+	pino({
+		level: 'info',
+		transport: {
+			target: 'pino-pretty',
+			options: {
+				colorize: true,
+				translateTime: 'HH:MM:ss',
+				ignore: 'pid,hostname',
+				messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+				hideObject: true,
+			},
+		},
+	}),
+);
+
+// Реалізований маршрут GET /notes
 app.get('/notes', (req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
+	console.log(req.body);
+	res.status(200).json({ message: 'Retrieved all notes' });
 });
 
+// Реалізований маршрут GET /notes/:noteId
 app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+	const noteId = req.params.noteId;
+
+	res.status(200).json({
+		message: `Retrieved note with ID: ${noteId}`
+	});
 });
 
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
+//Реалізований маршрут GET /test-error
+app.get('/test-error', (req, res) => {
+	throw new Error('Simulated server error');
 });
 
-// 404 Middleware
+// Middleware для обробки помилок 404
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Base route is here' });
+});
+
 app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
+	res.status(404).json({ message: 'Route not found' });
 });
 
-// Error Middleware
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message || 'Internal Server Error' });
+// Middleware для обробки помилок 500
+app.use((err, req, res, next) => {	//console.error('Error:', err.message);
+	res.status(500).json({
+		message: 'Simulated server error',
+
+	});
 });
 
+// Запуск сервера
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
