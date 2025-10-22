21e1f35f20ce74c7c0fb9abc28f270d37d3a37e8
Vlada <geazint6@gmail.com>
2025-10-22 16:51:51 +0300

    server.js  back

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
-import express from "express";
-import cors from "cors";
-import "dotenv/config";
-import { connectMongoDB } from "./db/connectMongoDB.js";
-import { logger } from "./middleware/logger.js";
-import { notFoundHandler } from "./middleware/notFoundHandler.js";
-import { errorHandler } from "./middleware/errorHandler.js";
-import notesRoutes from "./routes/notesRoutes.js";
-import { errors } from "celebrate";
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from './routes/notesRoutes.js';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -14,25 +14,21 @@ app.use(express.json());
 app.use(cors());
 app.use(logger);
 
-const PORT = process.env.PORT || 3030;
-
-// Root route
-app.get("/", (req, res) => {
-  res.send("✅ Validation API is running!");
-});
+const PORT = process.env.PORT ?? 3030;
 
 app.use(notesRoutes);
 
-// MW
+//MW
 app.use(notFoundHandler);
 app.use(errors());
 app.use(errorHandler);
 
-// 🟢Start server after DB connection
-try {
-  await connectMongoDB();
-  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-} catch (err) {
-  console.error("❌ Failed to connect to MongoDB:", err);
-  process.exit(1);
-}
+
+
+//DB
+await connectMongoDB();
+
+// Запуск сервера
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
